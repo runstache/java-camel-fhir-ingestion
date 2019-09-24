7c019e5581897414bb45504b61472e0d6dd2e3be
Larry Shields <shieldsl@upmc.edu>
2019-09-24 11:58:37 -0400

    Project Creation

---
+++ src/main/java/com/lswebworld/fhiringestion/FhirIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.lswebworld.fhiringestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirIngestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lswebworld/fhiringestion/FhirIngestionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lswebworld.fhiringestion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FhirIngestionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
