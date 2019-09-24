cad7e9c6d4e40ae86e64a26f3392d0f2ebc9c524
Larry Shields <shieldsl@upmc.edu>
2019-09-24 14:03:32 -0400

    Added Readme & Fhir Context Registration to Bean Factory based on FHIR Version provided.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# java-camel-fhir-ingestion
-Simple Camel FHIR Ingestion Engine to a FHIR Server from a File Store.
+# Camel FHIR Ingestion Engine
+
+Simple Camel Ingestion Engine for importing JSON FHIR Bundle files into a FHIR Server.
+
+## Configuration
+
+The following Configuration options are available via Environment Variables:
+
+* __ACTIVE_PROFILE__: This is the Spring Profile to use.
+* __LOG_LEVEL__: This is the Logging Level that should be set.
+* __APP_FHIRVERSION__: This is the version of FHIR that should be used for the communication with the FHIR Server.
+* __APP_SOURCEPATH__: This is the directory path for the FHIR Bundle JSON Files.
+* __APP_FHIRSERVER__: This is the url for the FHIR Server to post the Bundles to.
+* __APP_ENABLED__: Denotes to automatically start the FHIR Consumer Route automatically.
+* __APP_IDENTIFIEREXIST__: Denotes for the system to replace the Request section of the Bundle to use the PUT method and a URL that includes the first identifier on the resource.  If set to False, the existing Request section will be utilized on the Bundle Entry.  If set to True, it will be replaced.
--- src/main/java/com/lswebworld/fhiringestion/configuration/AppConfig.java
+++ src/main/java/com/lswebworld/fhiringestion/configuration/AppConfig.java
@@ -3,6 +3,7 @@ package com.lswebworld.fhiringestion.configuration;
 import ca.uhn.fhir.context.FhirContext;
 import com.lswebworld.fhiringestion.processors.RuleProcessor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,13 +12,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+  @Autowired
+  AppSettings settings;
+
+
   @Bean(name = "RuleProcessor")
   public RuleProcessor ruleProcessor() {
     return new RuleProcessor();
   }
 
+  /**
+   * Populates a Fhir Context into the Bean Factory based on the configured Fhir Version.
+   * @return FHIR Context.
+   */
   @Bean(name = "FhirContext")
   public FhirContext fhirContext() {
-    return FhirContext.forDstu3();
+    switch (settings.getFhirVersion().toLowerCase()) {
+      case "dstu2":
+        return FhirContext.forDstu2();
+      case "dstu3":
+        return FhirContext.forDstu3();
+      case "R4":
+        return FhirContext.forR4();
+      default:
+        return FhirContext.forDstu3();
+    }
   }
 }
